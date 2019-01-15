8a5bb33453e4805f64c36121b33e16967bb07d2b
YJC <cjchang23@gmail.com>
2019-01-15 18:01:27 +0800

    [leetcode] Refine printTree

--- customDataType.py
+++ customDataType.py
@@ -14,44 +14,29 @@ class TreeNode:
     @staticmethod
     def printTree(root):
 
-        depth = TreeNode.checkDepth(root, 0)
-        toCheck = [(root, 0)]
-        node_list = []
-        prv = 0
-        line = ""
-        while toCheck:
-            node, lvl = toCheck.pop(0)
-            if lvl > prv:
-                # print("lvl {}".format(lvl))
-                print("{}".format(line))
-                line = ""
-            prv = lvl
-            # if node:
-            #     node_list.append((str(node.val), lvl))
-            # else:
-            #     node_list.append(('N', lvl))
-            if node:
-                line += "".join([" " for _ in range((2 ** (depth - lvl)) - 1)])
-                line += str(node.val)
-                toCheck.append((node.left, lvl + 1))
-                toCheck.append((node.right, lvl + 1))
+        if root is None:
+            return
 
+        depth = TreeNode.checkDepth(root, 0)
+        node_list = [-1 for _ in range(2**depth-1)]
+        toCheck = [(root, 0)]
+        while toCheck:
+            node, parent_idx = toCheck.pop(0)
+            if node:
+                node_list[parent_idx] = node.val
+                toCheck.append((node.left, 2 * parent_idx + 1))
+                toCheck.append((node.right, 2 * parent_idx + 2))
 
         # cnt = 0
-        # for i in range(depth):
-        #     line = ""
-        #     for j in range(2**i):
-        #         # val = str(node_list[cnt].val) if node_list[cnt] else 'N'
-        #         # val = str(cnt)
-        #         # val = node_list[cnt]
-        #         # cnt += 1
-        #         if node_list[0][1] > i:
-        #             break
-        #         val, lvl = node_list.pop(0)
-        #         line += "".join([" " for _ in range((2 ** (depth - i)) - 1)])
-        #         line += "{}".format(val)
-        #
-        #     print(line)
+        for i in range(depth):
+            line = ""
+            for j in range(2**i):
+                idx = 2**i + j - 1
+                val = 'N' if node_list[idx] == -1 else str(node_list[idx])
+                line += "".join([" " for _ in range((2 ** (depth - i)) - 1)])
+                line += val #"{:02d}".format(idx)
+
+            print(line)
 
     @staticmethod
     def checkDepth(node, level_from):
